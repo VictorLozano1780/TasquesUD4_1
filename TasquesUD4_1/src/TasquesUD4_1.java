b85c569704d9307f251fb75ebd711e1fed8a0489
Víctor Lozano <55434666+VictorLozano1780@users.noreply.github.com>
2021-02-02 16:35:18 +0100

    Add files via upload

---
+++ TasquesUD4_1/src/TasquesUD4_1.java
@@ -0,0 +1,16 @@
+
+public class TasquesUD4_1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num1 = 30;
+		double num2 = 20;
+		
+		System.out.println("Suma: " + (num1 + num2));
+		System.out.println("Resta: " + (num1 - num2));
+		System.out.println("Multiplicación: " + (num1 * num2));
+		System.out.println("División: " + (num1 / num2));
+		System.out.println("Resto: " + (num1 % num2));
+	}
+
+}
